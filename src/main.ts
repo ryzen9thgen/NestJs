3e476ab2161d6871cff4d12d5f43f0d2bfb25ca8
ryzen9thgen <rarances@gbox.ncf.edu.ph>
2025-10-26 16:48:03 +0800

    Bind NestJS app to render port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+
+  // Listen on Render port
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
